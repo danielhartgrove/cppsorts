4ff922688b52cad521a15f2b17a9fafcc608d6fd
Dan Hartgrove <hartgrovedaniel3@outlook.com>
2023-06-06 18:58:46 +0100

    refactor(project): refactor the project into a Visual Studio Project
    Transfer code to Visual Studio

--- README.md
+++
@@ -1,8 +0,0 @@
-# cppsorts
-## a library for sorting algorithms in C++
-#### Authored by Daniel Hartgrove
-
-#### Features:
-- Bubble Sort
-- Selection Sort
-- Insertion Sort
---
+++ cppsorts/cppsorts.cpp
@@ -0,0 +1,60 @@
+#include <array>
+#include <stdio.h>
+#include <stdlib.h>
+
+bool swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+    return true;
+}
+
+int* bubbleSort(int arr[], int size) {
+    bool swapped; // we use a flag variable to check if a swap has been made in
+                  // the current iteration
+
+    for (int i = 0; i <= size; i++) {
+        swapped = false;
+        for (int j = 0; j <= size - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swapped = swap(
+                    &arr[j],
+                    &arr[j + 1]); // call the swap function, set the flag as the return
+                                  // value which tells you if it was successful
+            }
+        }
+
+        if (!swapped)
+            break;
+    }
+
+    return arr;
+}
+
+int* selectionSort(int arr[], int size) {
+    int min;
+    for (int i = 0; i <= size; i++) {
+        min = i;
+        for (int j = i + 1; j <= size; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        swap(&arr[min], &arr[i]);
+    }
+
+    return arr;
+}
+
+int main() {
+    int arr[] = { 1, 2, 3 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    bubbleSort(arr, size);
+
+    for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++) {
+        printf("%d", arr[i]);
+    }
+
+    return 0;
+}
